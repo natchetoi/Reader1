1ff09f05a6b3e05e071adde93dc4effe603bb8ea
Slava Bychkov <slavnost@gmail.com>
2017-01-31 22:42:10 +0300

    basic layout

--- www/view/Main.controller.js
+++ www/view/Main.controller.js
@@ -5,9 +5,9 @@ sap.ui.controller("Reader1.view.Main", {
 	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 	 * @memberOf Reader1.view.Main
 	 */
-	//	onInit: function() {
-	//
-	//	},
+		onInit: function() {	
+					
+		},
 
 	/**
 	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
